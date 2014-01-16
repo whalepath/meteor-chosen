47956da9d34de07f149ac8060bf2e9cae006b3e1
Patrick Moore <patmoore@farreach.es>
2014-01-16 14:36:57 -0800

    css files and images

--- package.js
+++ package.js
@@ -4,6 +4,11 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.add_files([
-        'chosen.proto.js'
+        'chosen.proto.js',
+        'chosen.proto.min.js',
+        'chosen.css',
+        'chosen.min.css',
+        'chosen-sprite.png',
+        'chosen-sprite@2x.png',
     ], 'client');
 });
